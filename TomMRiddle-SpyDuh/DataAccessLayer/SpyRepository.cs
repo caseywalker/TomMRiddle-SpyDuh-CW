ddb6a5cccde7db65e508b176c717ad926ab5ecde
Martin Sisk <Brothermj@hotmail.com>
2021-08-24 20:54:54 -0500

    commented out Skills and Services Methods in the SpyRepository.cs Class file.

--- TomMRiddle-SpyDuh/DataAccessLayer/SpyRepository.cs
+++ TomMRiddle-SpyDuh/DataAccessLayer/SpyRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using TomMRiddle_SpyDuh.Models;
 
 namespace TomMRiddle_SpyDuh.DataAccessLayer
@@ -57,14 +58,23 @@ namespace TomMRiddle_SpyDuh.DataAccessLayer
             _spies.Add(newSpy);
         }
 
-
         // Get by Id Method
         internal Spy GetById(Guid spyId)
         {
             return _spies.FirstOrDefault(spy => spy.SpyID == spyId);
         }
 
+        // Get by Skills
+        //internal IEnumerable<Spy> GetBySkill(LSTSkills skill)
+        //{
+        //    return _spies.Where(spy => spy.LSTSkills == skill);
+        //}
 
+        // Get by Services
+        //internal IEnumerable<Spy> GetByServices(List SpyServices services)
+        //{
+        //    return _spies.Where(spy => spy.SpyServices == services);
+        //}
 
   }
 }
