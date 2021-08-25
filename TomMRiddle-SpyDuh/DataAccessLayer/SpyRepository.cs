530db2e83a84eca4dd9b08a39127aac9d7b1811c
Caseywalker <casey.walker118@gmail.com>
2021-08-24 21:16:45 -0500

    Merging with main.

--- TomMRiddle-SpyDuh/DataAccessLayer/SpyRepository.cs
+++ TomMRiddle-SpyDuh/DataAccessLayer/SpyRepository.cs
@@ -11,41 +11,7 @@ namespace TomMRiddle_SpyDuh.DataAccessLayer
     {
 
     }
-    static List<Spy> _spies = new List<Spy>
-    {
-      new Spy
-      {
-        SpyID = Guid.NewGuid(),
-        Details = "Spy does good job",
-        SpyBackground = "Spy been doin this a long time",
-        LSTSkills = new List<string>
-        {
-          "Spying",
-          "Plotting"
-        },
-        SpyServices = new List<string>
-        {
-          "Espionage",
-          "Political take-over"
-        },
-      },
-      new Spy
-      {
-        SpyID = Guid.NewGuid(),
-        Details = "Spy does decent job",
-        SpyBackground = "Spy been doin this a little time",
-        LSTSkills = new List<string>
-        {
-          "Spying",
-          "Infiltrating"
-        },
-        SpyServices = new List<string>
-        {
-          "Trapping",
-          "Influence"
-        }
-      },
-    };
+    static List<Spy> _spies = new List<Spy>();
         
         // GetAll() return _spies field
         internal IEnumerable<Spy> GetAll()
